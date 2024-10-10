8f6cf4cfbf4e349a9c96ffe058375a8cc34aaf33
kozak <kozak@192.168.1.5>
2024-10-10 16:06:58 +0300

    Integrated rollbar

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,8 +1,9 @@
 import {useEffect} from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Provider as ReduxProvider } from 'react-redux';
 import { ToastContainer, toast } from 'react-toastify';
 import {useTranslation} from "react-i18next";
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { Login, Chat, NotFound, Signup } from './pages';
 import { Layout } from './';
 import store from '../store';
@@ -12,6 +13,11 @@ function App() {
     const { t } = useTranslation();
     const networkState = useNetworkState();
 
+    const rollbarConfig = {
+        accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+        environment: process.env.REACT_APP_ROLLBAR_ENVIRONMENT,
+    };
+
     useEffect(() => {
         if (!networkState) {
             toast.error(t('app.offlineToastText'));
@@ -19,21 +25,23 @@ function App() {
     }, [networkState]);
 
     return (
-      <>
-          <Provider store={store}>
-              <Layout>
-                  <BrowserRouter>
-                      <Routes>
-                          <Route path="/" element={<Chat />} />
-                          <Route path="login" element={<Login />} />
-                          <Route path="signup" element={<Signup />} />
-                          <Route path="*" element={<NotFound />} />
-                      </Routes>
-                  </BrowserRouter>
-              </Layout>
-          </Provider>
-          <ToastContainer />
-      </>
+      <RollbarProvider config={rollbarConfig}>
+          <ErrorBoundary>
+              <ReduxProvider store={store}>
+                  <Layout>
+                      <BrowserRouter>
+                          <Routes>
+                              <Route path="/" element={<Chat />} />
+                              <Route path="login" element={<Login />} />
+                              <Route path="signup" element={<Signup />} />
+                              <Route path="*" element={<NotFound />} />
+                          </Routes>
+                      </BrowserRouter>
+                  </Layout>
+              </ReduxProvider>
+              <ToastContainer />
+          </ErrorBoundary>
+      </RollbarProvider>
     );
 }
 
